026b6d39872fbdc5b16e34876b6576856ce5b7ac
toxicant123 <13655847550@163.com>
2024-04-14 14:04:16 +0800

    Python 0.0.4

--- Python/src/第一章/4.py
+++ Python/src/第一章/4.py
@@ -1,4 +1,14 @@
+year = 2024  # 年份
+month = 4
+day = 20
+week = '日'
+weather = '晴'
+
 print('我是mia')
-print()
-year = 2024
-print(year, '年我要减肥', sep='')
+
+print(year, '年，我要减肥', sep='', end='\n\n')
+print(year, '年，我要读100本书', sep='', end='\n\n')
+print(year, '年，我要去10个城市旅游', sep='', end='\n\n')
+
+print('今天是 %d 年 %2d 月 %2d 日，星期%s，天气%s，温度%.2f' % (year, month, day, week, weather, 31.0))
+444
