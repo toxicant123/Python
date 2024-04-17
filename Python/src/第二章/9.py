b022199daacff8b86b1c943504dbfc8502d905d3
toxicant123 <13655847550@163.com>
2024-04-17 21:43:03 +0800

    Python 0.0.16

--- Python/src/第二章/9.py
+++ Python/src/第二章/9.py
@@ -3,9 +3,51 @@ s1 = '2024'
 print(s1)
 n = int(s1)
 print(n)
+print(type(s1), type(n))
+
+# s2 = 'aaa'
+# print(int(s2))
+
+b1, b2 = True, False
+print(int(b1), int(b2))
 
 # 转换为浮点数
 
+s3 = '0.045'
+f1 = float(s3)
+print(f1)
+
+n2 = 2024
+f2 = float(n2)
+print(f2)
+
 # 转换为布尔
 
+s4 = 'abc'
+b3 = bool(s4)
+print(b3)
+
+s5 = ''
+b4 = bool(s5)
+print(b4)
+
+n3 = 0
+b5 = bool(n3)
+print(b5)
+
+s6 = '0'
+b6 = bool(s6)
+print(b6)
+
+n4 = 1
+b7 = bool(n4)
+print(b7)
+
 # 转换为字符串
+
+n5 = 0
+s7 = str(n5)
+
+f3 = 0.010101010101010101010101
+s8 = str(f3)
+print(s8)
