7aa3035945f143419e4db468bf50e829a5617bd3
toxicant123 <13655847550@163.com>
2024-04-17 23:33:52 +0800

    Python 0.0.19

---
+++ Python/src/第二章/11.py
@@ -0,0 +1,7 @@
+total = 10
+a = 2
+b = 4
+a_and_b = a + b
+left = total - a_and_b
+
+print('小A和小B两个人一共拿走了%d个苹果，小C能拿走%d个苹果' % (a_and_b, left))
---
+++ Python/src/第二章/12.py
@@ -0,0 +1,10 @@
+total = 100
+
+total += 10
+print('账户剩余%d元' % total)
+
+total -= 20
+print('账户剩余%d元' % total)
+
+total -= total
+print('账户剩余%d元' % total)
