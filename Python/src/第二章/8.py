9b3974f2df5ec2d85960e20b8bc0c6d9b16ef257
toxicant123 <13655847550@163.com>
2024-04-17 21:22:18 +0800

    Python 0.0.15

---
+++ Python/src/第二章/8.py
@@ -0,0 +1,20 @@
+str = 'abcde'
+
+print(str[0])
+print(str[1])
+print(str[2])
+print(str[3])
+print(str[4])
+print(str[-1])
+print(str[-2])
+print(str[-3])
+print(str[-4])
+print(str[-5])
+
+# 切片
+print(str[0:3])
+print(str[0:5:2])
+
+print(str[-1:-6:-1])
+print(str[::-1])
+print(str[::-2])
