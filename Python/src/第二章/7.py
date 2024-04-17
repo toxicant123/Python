f6c45f316e78f1d898f7fa47939b263fcaf2f5b0
toxicant123 <13655847550@163.com>
2024-04-17 21:00:21 +0800

    Python 0.0.14

---
+++ Python/src/第二章/7.py
@@ -0,0 +1,17 @@
+s1 = 'hello'
+s2 = 'world'
+print(s1 + ' ' + s2)
+
+s3 = '''
+hello, world
+'''
+print(s3)
+
+s4 = 'It\'s a string'
+print(s4)
+
+s5 = 'a'
+print(s5 * 6)
+
+# print(s1 + s2 + s3 + s4 + s5)
+
