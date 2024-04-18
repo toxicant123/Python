52e4c0f47f8947cd85f25be398c18bff653126d8
toxicant123 <13655847550@163.com>
2024-04-18 23:06:29 +0800

    Python 0.0.22

---
+++ Python/src/第三章/4.py
@@ -0,0 +1,20 @@
+print(True and True)
+print(True or False)
+print(True and False)
+print(False or True)
+print(False or False)
+
+a = True and True
+print(a)
+
+print(True or False and True)
+print('hello' or 'hi')
+print('hello' and 'hi')
+print(2024 or 2025 or 0)
+print(2024 and 2025 and 0)
+print(2024 and 2025 or 0)
+
+print(not True)
+print(not 0)
+print(not 1)
+print(not '')
