c6263e718480bffabf12dffd3fd8d38a36c757f5
toxicant123 <13655847550@163.com>
2024-04-20 09:27:41 +0800

    Python 0.0.25

---
+++ Python/src/第三章/7.py
@@ -0,0 +1,14 @@
+print('' < "a")
+
+print('x' > 'y' == False)
+
+total = 14
+stu = 4
+
+each_stu = 14 // 4
+print(each_stu)
+
+given_apple = stu * each_stu
+print(given_apple)
+
+print(total - given_apple)
