5864e66b21406990670ab0a81c91c72ef62b392e
toxicant123 <13655847550@163.com>
2024-04-22 21:52:28 +0800

    Python 0.0.29

---
+++ Python/src/第四章/2.py
@@ -0,0 +1,14 @@
+score = input('请输入你的成绩')
+
+score = int(score)
+
+if score >= 90:
+    print('LEVEL A')
+elif score >= 80:
+    print('LEVEL B')
+elif score >= 70:
+    print('LEVEL C')
+elif score >= 60:
+    print('LEVEL D')
+else:
+    print('NO LEVEL')
