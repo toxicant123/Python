6d2462c262b268a33431784420d164f8ce685bfb
toxicant123 <13655847550@163.com>
2024-04-22 20:57:28 +0800

    Python 0.0.28

---
+++ Python/src/第四章/1.py
@@ -0,0 +1,18 @@
+weather = '下雨'
+
+if weather == '下雨':
+    print('需要带伞')
+
+amb = 0
+
+if amb == 0:
+    print('买伞')
+else:
+    print('不用买')
+
+age = input('你多大了？')
+
+if int(age) >= 18:
+    print('可以阅读')
+else:
+    print('您尚未满足条件')
