a51642b64f8ada0ca4cb825cd4bd2a135d562ce7
toxicant123 <13655847550@163.com>
2024-04-27 14:01:09 +0800

    Python 0.0.48

---
+++ Python/src/第六章/6.py
@@ -0,0 +1,18 @@
+tuple1 = (1, 2, 3, True, 'Hello')
+
+print(tuple1)
+
+print(tuple1[1])
+
+tuple2 = ()
+
+print(type(tuple2))
+
+tuple3 = (1)
+
+print(tuple3, type(tuple3))
+
+list1 = list(tuple1)
+
+print(list1)
+
