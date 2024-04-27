b6a151173f6f3d4b4f0764deffcba3836133b8ef
toxicant123 <13655847550@163.com>
2024-04-27 10:44:59 +0800

    Python 0.0.45

---
+++ Python/src/第六章/4.py
@@ -0,0 +1,12 @@
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for item in list1:
+    print(item)
+
+for i, k in enumerate(list1):
+    print(i, k)
+
+for index in range(len(list1)):
+    print(list1[index])
+
+
