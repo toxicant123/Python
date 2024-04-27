c31cf45d0de7307887ed17996eff138c20fb9ba4
toxicant123 <13655847550@163.com>
2024-04-27 10:20:27 +0800

    Python 0.0.43

---
+++ Python/src/第六章/1.py
@@ -0,0 +1,18 @@
+list1 = [1, 2, 3, 4, 5, 6]
+
+print(list1)
+
+print(type(list1))
+
+list2 = [1,2,3,True, False, 'hello']
+
+print(list2)
+
+list3 = list()
+
+print(list3)
+
+list4 = list('abcdefg')
+
+print(list4)
+
---
+++ Python/src/第六章/2.py
@@ -0,0 +1,18 @@
+s = 'abcdefg'
+
+str_list = list(s)
+
+print(str_list[1])
+
+list2 = [1, 2, 3, 4, 5, 6, 7, 8]
+
+print(list2[2:6:2])
+
+print(str_list + list2)
+
+print(list2 * 4)
+
+print(0 in list2)
+
+print('a' in str_list)
+
