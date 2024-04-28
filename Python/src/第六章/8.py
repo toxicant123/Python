fc9fa565a2a3ef7bb1b9351a82cb0b2c524127c5
toxicant123 <13655847550@163.com>
2024-04-28 21:41:49 +0800

    Python 0.0.48

---
+++ Python/src/第六章/8.py
@@ -0,0 +1,19 @@
+tuple1 = (1, 2, 3)
+
+tuple2 = (4, 5, 6)
+
+print(tuple1 + tuple2)
+
+print(tuple1.count(1))
+
+print(tuple1.index(2))
+
+list1 = [1, 2, 3]
+
+print(list1.index(4))
+
+print('222')
+
+
+
+
