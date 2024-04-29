ccdca0d89a5f6e8be0f466781ff8655b1449432c
toxicant123 <13655847550@163.com>
2024-04-29 23:51:29 +0800

    Python 0.0.53

---
+++ Python/src/第六章/13.py
@@ -0,0 +1,21 @@
+d = {
+    'name': 'hxf',
+    'age': 30,
+    'gender': False,
+    'money': 0
+}
+
+print(d)
+
+print(d['name'])
+
+print(type(d))
+
+d = {}
+
+print(type(d))
+
+d = dict()
+
+print(type(d))
+
