e2d506827e791f8bad2a695c4e44db106900fe31
toxicant123 <13655847550@163.com>
2024-04-29 22:31:33 +0800

    Python 0.0.51

---
+++ Python/src/第六章/12.py
@@ -0,0 +1,16 @@
+s1 = 'hello world'
+
+print(s1 + ' mia')
+
+print(s1 * 3)
+
+print(len(s1))
+
+print(max(s1), min(s1))
+
+print(str([1, 2, 3, 4]))
+
+print(s1.islower())
+print(s1.isupper())
+print(s1.count('o'))
+print(s1.strip())
