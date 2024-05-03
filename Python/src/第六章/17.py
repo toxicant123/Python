238381f67091f1e62551d7760d00f5288adcb8d9
toxicant123 <13655847550@163.com>
2024-05-03 09:38:23 +0800

    Python 0.0.60

---
+++ Python/src/第六章/17.py
@@ -0,0 +1,21 @@
+list1 = [1, 2, 3, 4, 5]
+
+list1.extend([6, 7, 8, 9])
+
+print(list1)
+
+list1.append(10)
+
+print(list1)
+
+d1 = {
+    'a': 1,
+    'b': 2,
+    'c': 3
+}
+
+print(d1.get('d'))
+
+s = 'Python'
+
+print(s[0:len(s)])
