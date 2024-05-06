3fafa407c44205b7fe3be3a536566a8ee14def3b
toxicant123 <13655847550@163.com>
2024-05-07 00:03:52 +0800

    Python 0.0.67

--- Python/src/第七章/6.py
+++ Python/src/第七章/6.py
@@ -1,13 +1,26 @@
 while True:
-    op = input('请输入一个四则运算式：')
-    if '加法':
-        pass
-    elif '减法':
-        pass
-    elif '乘法':
-        pass
-    elif '除法':
-        pass
-    elif op == 'C':
-        print('感谢您使用本计算器')
-        break
+    try:
+        op = input('请输入一个四则运算式：')
+        if '+' in op:
+            a = op.split('+')
+            result = int(a[0]) + int(a[1])
+            print(result)
+        elif '-' in op:
+            a = op.split('-')
+            result = int(a[0]) - int(a[1])
+            print(result)
+        elif '*' in op:
+            a = op.split('*')
+            result = int(a[0]) * int(a[1])
+            print(result)
+        elif '/' in op:
+            a = op.split('/')
+            result = int(a[0]) / int(a[1])
+            print(result)
+        elif op == 'C':
+            print('感谢您使用本计算器')
+            break
+        else:
+            raise Exception("不合法的输入：%s" % op)
+    except Exception as e:
+        print(e)
