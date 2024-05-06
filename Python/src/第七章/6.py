6261a0ce88e2117fddc431887ccd0b57234812a4
toxicant123 <13655847550@163.com>
2024-05-06 23:55:09 +0800

    Python 0.0.66

---
+++ Python/src/第七章/6.py
@@ -0,0 +1,13 @@
+while True:
+    op = input('请输入一个四则运算式：')
+    if '加法':
+        pass
+    elif '减法':
+        pass
+    elif '乘法':
+        pass
+    elif '除法':
+        pass
+    elif op == 'C':
+        print('感谢您使用本计算器')
+        break
