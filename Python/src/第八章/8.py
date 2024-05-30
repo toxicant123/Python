ac56a39b8ac2adffb16ff9ee26ce81709235fce7
toxicant123 <13655847550@163.com>
2024-05-30 23:25:27 +0800

    Python 0.0.96

---
+++ Python/src/第八章/8.py
@@ -0,0 +1,18 @@
+def total(*a):
+    result = 0
+    for i in a:
+        result += i
+    return result
+
+
+print(total(1, 2, 3, 4, 5))
+
+
+def f(**kwargs):
+    for key, value in kwargs.items():
+        print(key, value)
+
+
+f(**{
+    'a': 1, 'b': 2, 'c': 3
+})
