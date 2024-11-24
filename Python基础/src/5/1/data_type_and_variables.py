5f4c629a89ceb8b330b8f0d5e6edc4e28e09a8f6
toxicant123 <13655847550@163.com>
2024-11-24 13:01:02 +0800

    Python 0.1.2

---
+++ Python基础/src/5/1/data_type_and_variables.py
@@ -0,0 +1,24 @@
+num1 = 100
+num2 = 0xff00
+num3 = 10_000_000
+
+print(num1, num2, num3)
+
+float1 = 1.23
+float2 = 3.5e9
+float3 = 1.4e-6
+
+print(float1, float2, float3)
+
+str1 = 'a'
+str2 = "b"
+str3 = '"'
+str4 = "I'm ok"
+str5 = 'you\'re ok?'
+str6 = 'a\nb\nc'
+str7 = r'a\nb\nc'
+str8 = '''a1
+b2
+c3'''
+
+print(str1, str2, str3, str4, str5, str6, str7, str8)
