cc94678d4dc92f05835d42f4703064dea39b4b14
toxicant123 <13655847550@163.com>
2024-12-27 01:12:01 +0800

    Python 0.1.29

--- Python基础/src/6/4/recursive_function.py
+++ Python基础/src/6/4/recursive_function.py
@@ -5,3 +5,14 @@ def fact(n):
 
 print(fact(5))
 print(fact(100))
+
+
+def move(n: int, a: str, b: str, c: str) -> None:
+    if n == 1:
+        print(a, '-->', c)
+        return
+    move(n - 1, a, c, b)
+    move(1, a, b, c)
+    move(n - 1, b, a, c)
+
+move(3, 'A', 'B', 'C')
